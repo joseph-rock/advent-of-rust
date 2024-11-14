dc71bc5fcf88b58391c4d68e47289d84553c3083
Joseph <joseph@joseph-rock.com>
2024-11-14 14:34:10 -0800

    Pt1 Complete

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 
 #[derive(Debug)]
 struct Card {
-    id: String,
+    _id: String,
     winning_numbers: Vec<u8>,
     numbers_you_have: Vec<u8>,
 }
@@ -10,12 +10,16 @@ struct Card {
 fn main() {
     let input: &str = include_str!("./day4.txt");
     let pt1 = part_1(input);
+    println!("Part 1: {}", pt1);
 }
 
 fn part_1(input: &str) -> usize {
-    let cards: Vec<Card> = input.lines().map(|line| parse_card(line)).collect();
-    dbg!(cards);
-    todo!();
+    input
+        .lines()
+        .map(|line| parse_card(line))
+        .map(|card| matched_numbers(&card))
+        .map(|matched_numbers| score(matched_numbers.len()))
+        .fold(0, |sum, score| sum + score)
 }
 
 fn parse_card(line: &str) -> Card {
@@ -30,12 +34,12 @@ fn parse_card(line: &str) -> Card {
     .unwrap();
 
     let groups = re.captures(line).unwrap();
-    let id = groups["id"].to_string();
+    let _id = groups["id"].to_string();
     let winning_numbers = to_u8_vec(&groups["winning_numbers"].to_string());
     let numbers_you_have = to_u8_vec(&groups["numbers_you_have"].to_string());
 
     Card {
-        id,
+        _id,
         winning_numbers,
         numbers_you_have,
     }
@@ -46,4 +50,20 @@ fn to_u8_vec(input: &str) -> Vec<u8> {
         .split_whitespace()
         .map(|num| num.parse::<u8>().unwrap())
         .collect()
+}
+
+fn matched_numbers(card: &Card) -> Vec<u8> {
+    card.numbers_you_have
+        .clone()
+        .into_iter()
+        .filter(|num| card.winning_numbers.contains(num))
+        .collect()
+}
+
+fn score(n: usize) -> usize {
+    if n <= 0 {
+        return 0;
+    }
+    let power = (n - 1) as u32;
+    usize::pow(2, power)
 }
